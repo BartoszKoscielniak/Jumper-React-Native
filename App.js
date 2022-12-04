c8260fcdcc6ce6ba94245d40ac9896559ff72f89
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2022-12-04 01:49:09 +0100

    ADD: game over screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Dimensions, Image, Text, TouchableOpacity, View} from 'react-native';
 import { GameEngine } from 'react-native-game-engine'; 
 import entities from './entities';
 import Physics from './utils/physics';
@@ -11,15 +11,17 @@ const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 
 export default function App() {
-  const [running, setRunning] = useState(false)
-  const [gameEngine, setGameEngine] = useState(null)
-  const [points, setPoints] = useState(0)
+    const [running, setRunning] = useState(false)
+    const [gameEngine, setGameEngine] = useState(null)
+    const [points, setPoints] = useState(0)
+    const [gameStatus, setGameStatus] = useState(null)
 
-  useEffect(() => {
-    setRunning(false)
-  }, [])
+    useEffect(() => {
+        setRunning(false)
+    }, [])
 
-  return (
+    return (
+
     <View style={{flex: 1}}>
         <Image
             source={backgroundBrown}
@@ -34,8 +36,11 @@ export default function App() {
             }}
             resizeMode={"repeat"}
         />
-      <Text style={{textAlign: 'center', fontSize: 50, fontWeight: 'bold', margin: 20}}>{points}</Text>
-      <GameEngine 
+        {}
+        { running ?
+        <Text style={{textAlign: 'center', fontSize: 50, fontWeight: 'bold', margin: 20}}>{points}</Text>
+        : null}
+      <GameEngine
       ref={(ref) => { setGameEngine(ref) }}
       systems={[Physics]}
       entities={entities()}
@@ -44,6 +49,7 @@ export default function App() {
         switch(e.type){
           case 'game_over':
             setRunning(false)
+            setGameStatus(e.type)
             gameEngine.stop()
             break;
           case 'new_point':
@@ -55,8 +61,7 @@ export default function App() {
       >
       <StatusBar style="auto" hidden={true}/>
       </GameEngine>
-
-      {!running ? 
+      {!running && gameStatus === null?
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
           <TouchableOpacity style={{backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10}}
           onPress={ () => {
@@ -69,6 +74,24 @@ export default function App() {
             </Text>
           </TouchableOpacity>
       </View> : null}
+        {!running && gameStatus === 'game_over' ?
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <TouchableOpacity style={{backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10}}
+          onPress={ () => {
+            setPoints(0)
+            setRunning(true)
+            gameEngine.swap(entities())
+          }}>
+            <Text style={{fontWeight: 'bold', color: 'white', fontSize: 30}}>
+              GAME OVER
+            </Text>
+              <Text style={{fontWeight: 'bold', color: 'white', fontSize: 30, textAlign: "center"}}>
+                  Points: {points}
+            </Text>
+          </TouchableOpacity>
+      </View> : null}
+
     </View>
+
   );
 }
